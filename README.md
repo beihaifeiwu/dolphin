3ff84e21ad08f062ceec005c464bffef98c951bb
liu pin <pin.liu@palmaplus.com>
2015-02-14 09:59:14 +0800

    add license

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dolphin 
+# dolphin
+
+# License
+The Dolphin project is released under version 2.0 of the [Apache License](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/comment/CommentGenerator.java
@@ -0,0 +1,33 @@
+package com.freetmp.mbg.comment;
+
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * Created by LiuPin on 2015/2/14.
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+
+    public CommentGenerator() {
+        super();
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        compilationUnit.addFileCommentLine(" Copyright 2014-2015 the original author or authors.");
+        compilationUnit.addFileCommentLine("\n");
+        compilationUnit.addFileCommentLine(" Licensed under the Apache License, Version 2.0 (the \"License\");");
+        compilationUnit.addFileCommentLine(" you may not use this file except in compliance with the License.");
+        compilationUnit.addFileCommentLine(" You may obtain a copy of the License at");
+        compilationUnit.addFileCommentLine("\n");
+        compilationUnit.addFileCommentLine("   http://www.apache.org/licenses/LICENSE-2.0");
+        compilationUnit.addFileCommentLine("\n");
+        compilationUnit.addFileCommentLine(" Unless required by applicable law or agreed to in writing, software");
+        compilationUnit.addFileCommentLine(" distributed under the License is distributed on an \"AS IS\" BASIS,");
+        compilationUnit.addFileCommentLine(" WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.");
+        compilationUnit.addFileCommentLine(" See the License for the specific language governing permissions and");
+        compilationUnit.addFileCommentLine(" limitations under the License.");
+    }
+
+    
+}
