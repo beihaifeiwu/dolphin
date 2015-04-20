ba25354a593d127c60391618b2814de2fc9a6541
liu pin <pin.liu@palmaplus.com>
2015-04-20 12:52:33 +0800

    add default expression merger

---
+++ dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/merge/expression/ExpressionMerger.java
@@ -0,0 +1,29 @@
+package com.freetmp.mbg.merge.expression;
+
+import com.freetmp.mbg.merge.AbstractMerger;
+import com.github.javaparser.ast.expr.Expression;
+
+/**
+ * Created by LiuPin on 2015/4/20.
+ */
+public class ExpressionMerger extends AbstractMerger<Expression> {
+
+    private ExpressionMerger(){}
+
+    static {
+        if(getMerger(Expression.class) == null){
+            register(Expression.class,new ExpressionMerger());
+        }
+    }
+
+    @Override public Expression merge(Expression first, Expression second) {
+        return first;
+    }
+
+    @Override public boolean isEquals(Expression first, Expression second) {
+        if(first == second) return true;
+        if(first == null || second == null) return false;
+
+        return first.equals(second);
+    }
+}
