7a1e224a5e29a16403bb84102b1ce9bd44a4a648
liu pin <pin.liu@palmaplus.com>
2015-05-21 17:30:56 +0800

    add convenient method for Java Source generate

--- dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/plugin/AbstractXmbgPlugin.java
+++ dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/plugin/AbstractXmbgPlugin.java
@@ -3,6 +3,7 @@ package com.freetmp.mbg.plugin;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.Element;
 import org.mybatis.generator.api.dom.xml.TextElement;
@@ -16,28 +17,32 @@ import java.util.List;
  */
 public abstract class AbstractXmbgPlugin extends PluginAdapter {
 
-  protected  void generateTextBlock(String text, XmlElement parent){
+  /******************
+   * Generate Helper Method For Xml Mapper
+   *******************/
+
+  protected void generateTextBlock(String text, XmlElement parent) {
     parent.addElement(new TextElement(text));
   }
 
-  protected void generateTextBlockAppendTableName(String text,IntrospectedTable introspectedTable, XmlElement parent){
+  protected void generateTextBlockAppendTableName(String text, IntrospectedTable introspectedTable, XmlElement parent) {
     StringBuilder sb = new StringBuilder();
     sb.append(text);
     sb.append(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime());
     parent.addElement(new TextElement(sb.toString()));
   }
 
-  protected void generateParameterForSet(List<IntrospectedColumn> columns, XmlElement parent){
-    generateParameterForSet("",false,columns,parent);
+  protected void generateParameterForSet(List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParameterForSet("", false, columns, parent);
   }
 
-  protected void generateParameterForSet(String fieldPrefix,List<IntrospectedColumn> columns, XmlElement parent){
-    generateParameterForSet(fieldPrefix,false,columns,parent);
+  protected void generateParameterForSet(String fieldPrefix, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParameterForSet(fieldPrefix, false, columns, parent);
   }
 
-  protected void generateParameterForSet(String fieldPrefix,boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement dynamicElement) {
+  protected void generateParameterForSet(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement dynamicElement) {
     XmlElement trimElement = new XmlElement("trim");
-    trimElement.addAttribute(new Attribute("suffixOverrides",","));
+    trimElement.addAttribute(new Attribute("suffixOverrides", ","));
 
     StringBuilder sb = new StringBuilder();
     for (IntrospectedColumn introspectedColumn : columns) {
@@ -55,9 +60,9 @@ public abstract class AbstractXmbgPlugin extends PluginAdapter {
 
   protected void doIfNullCheck(String fieldPrefix, boolean ifNullCheck, XmlElement trimElement, StringBuilder sb, IntrospectedColumn introspectedColumn) {
     Element content;
-    if(ifNullCheck) {
+    if (ifNullCheck) {
       content = wrapIfNullCheckForJavaProperty(fieldPrefix, new TextElement(sb.toString()), introspectedColumn);
-    }else {
+    } else {
       content = new TextElement(sb.toString());
     }
     trimElement.addElement(content);
@@ -74,24 +79,24 @@ public abstract class AbstractXmbgPlugin extends PluginAdapter {
     return isNotNullElement;
   }
 
-  protected void generateParametersSeparateByComma(List<IntrospectedColumn> columns, XmlElement parent){
-    generateParametersSeparateByComma("",false,columns,parent);
+  protected void generateParametersSeparateByComma(List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByComma("", false, columns, parent);
   }
 
-  protected void generateParametersSeparateByComma(String fieldPrefix,List<IntrospectedColumn> columns, XmlElement parent){
-    generateParametersSeparateByComma(fieldPrefix,false,columns,parent);
+  protected void generateParametersSeparateByComma(String fieldPrefix, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByComma(fieldPrefix, false, columns, parent);
   }
 
-  protected void generateParametersSeparateByComma(String fieldPrefix,boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
-    generateParametersSeparateByComma(fieldPrefix,ifNullCheck,false,columns,parent);
+  protected void generateParametersSeparateByComma(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByComma(fieldPrefix, ifNullCheck, false, columns, parent);
   }
 
-  protected void generateParametersSeparateByComma(String fieldPrefix,boolean ifNullCheck,boolean withParenthesis, List<IntrospectedColumn> columns, XmlElement parent) {
+  protected void generateParametersSeparateByComma(String fieldPrefix, boolean ifNullCheck, boolean withParenthesis, List<IntrospectedColumn> columns, XmlElement parent) {
     XmlElement trimElement = new XmlElement("trim");
-    trimElement.addAttribute(new Attribute("suffixOverrides",","));
-    if(withParenthesis){
-      trimElement.addAttribute(new Attribute("prefix","("));
-      trimElement.addAttribute(new Attribute("suffix",")"));
+    trimElement.addAttribute(new Attribute("suffixOverrides", ","));
+    if (withParenthesis) {
+      trimElement.addAttribute(new Attribute("prefix", "("));
+      trimElement.addAttribute(new Attribute("suffix", ")"));
     }
 
     StringBuilder sb = new StringBuilder();
@@ -105,31 +110,31 @@ public abstract class AbstractXmbgPlugin extends PluginAdapter {
     parent.addElement(trimElement);
   }
 
-  protected void generateParametersSeparateByCommaWithParenthesis(List<IntrospectedColumn> columns, XmlElement parent){
-    generateParametersSeparateByCommaWithParenthesis("",false,columns,parent);
+  protected void generateParametersSeparateByCommaWithParenthesis(List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByCommaWithParenthesis("", false, columns, parent);
   }
 
-  protected void generateParametersSeparateByCommaWithParenthesis(String fieldPrefix,List<IntrospectedColumn> columns, XmlElement parent){
-    generateParametersSeparateByCommaWithParenthesis(fieldPrefix,false,columns,parent);
+  protected void generateParametersSeparateByCommaWithParenthesis(String fieldPrefix, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByCommaWithParenthesis(fieldPrefix, false, columns, parent);
   }
 
-  protected void generateParametersSeparateByCommaWithParenthesis(String fieldPrefix,boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
-    generateParametersSeparateByComma(fieldPrefix,ifNullCheck,true,columns,parent);
+  protected void generateParametersSeparateByCommaWithParenthesis(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateParametersSeparateByComma(fieldPrefix, ifNullCheck, true, columns, parent);
   }
 
-  protected void generateActualColumnNamesWithParenthesis(List<IntrospectedColumn> columns, XmlElement parent){
-    generateActualColumnNamesWithParenthesis("",false,columns,parent);
+  protected void generateActualColumnNamesWithParenthesis(List<IntrospectedColumn> columns, XmlElement parent) {
+    generateActualColumnNamesWithParenthesis("", false, columns, parent);
   }
 
-  protected void generateActualColumnNamesWithParenthesis(String fieldPrefix,boolean ifNullCheck,List<IntrospectedColumn> columns, XmlElement parent) {
-    generateActualColumnNamesWithParenthesis(fieldPrefix, null,ifNullCheck, columns, parent);
+  protected void generateActualColumnNamesWithParenthesis(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateActualColumnNamesWithParenthesis(fieldPrefix, null, ifNullCheck, columns, parent);
   }
 
-  protected void generateActualColumnNamesWithParenthesis(String fieldPrefix,String columnPrefix,boolean ifNullCheck,List<IntrospectedColumn> columns, XmlElement parent) {
+  protected void generateActualColumnNamesWithParenthesis(String fieldPrefix, String columnPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
     XmlElement trimElement = new XmlElement("trim");
-    trimElement.addAttribute(new Attribute("suffixOverrides",","));
-    trimElement.addAttribute(new Attribute("prefix","("));
-    trimElement.addAttribute(new Attribute("suffix",")"));
+    trimElement.addAttribute(new Attribute("suffixOverrides", ","));
+    trimElement.addAttribute(new Attribute("prefix", "("));
+    trimElement.addAttribute(new Attribute("suffix", ")"));
 
     StringBuilder sb = new StringBuilder();
     for (IntrospectedColumn introspectedColumn : columns) {
@@ -143,18 +148,17 @@ public abstract class AbstractXmbgPlugin extends PluginAdapter {
     parent.addElement(trimElement);
   }
 
-
-  protected void generateWhereConditions(String fieldPrefix, List<IntrospectedColumn> columns, XmlElement parent){
-    generateWhereConditions(fieldPrefix,false,columns,parent);
+  protected void generateWhereConditions(String fieldPrefix, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateWhereConditions(fieldPrefix, false, columns, parent);
   }
 
-  protected void generateWhereConditions(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent){
-    generateWhereConditions(fieldPrefix,null,ifNullCheck,columns,parent);
+  protected void generateWhereConditions(String fieldPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
+    generateWhereConditions(fieldPrefix, null, ifNullCheck, columns, parent);
   }
 
-  protected void generateWhereConditions(String fieldPrefix, String columnPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent){
+  protected void generateWhereConditions(String fieldPrefix, String columnPrefix, boolean ifNullCheck, List<IntrospectedColumn> columns, XmlElement parent) {
     XmlElement trimElement = new XmlElement("trim");
-    trimElement.addAttribute(new Attribute("suffixOverrides",","));
+    trimElement.addAttribute(new Attribute("suffixOverrides", ","));
 
     StringBuilder sb = new StringBuilder();
     for (IntrospectedColumn introspectedColumn : columns) {
@@ -170,5 +174,153 @@ public abstract class AbstractXmbgPlugin extends PluginAdapter {
     XmlElement where = new XmlElement("where");
     where.addElement(trimElement);
     parent.addElement(where);
+  }
+
+  /******************
+   * Generate Helper Method For Java Source
+   *******************/
+
+  protected void generateGetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    Method method = new Method();
+    method.setVisibility(JavaVisibility.PUBLIC);
+    method.setName("get" + capitalize(field));
+    method.setReturnType(type);
+    method.addBodyLine("return this." + field + ";");
+    innerClass.addMethod(method);
+  }
+
+  protected void generateSetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    Method method = new Method();
+    method.setVisibility(JavaVisibility.PUBLIC);
+    method.setName("set" + capitalize(field));
+    method.addParameter(new Parameter(type, field));
+    method.addBodyLine("this." + field + " = " + field + ";");
+    innerClass.addMethod(method);
+  }
+
+  protected void generateGetterSetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    generateSetterFor(field, type, innerClass);
+    generateGetterFor(field, type, innerClass);
+  }
+
+  protected Field generateFieldDeclarationFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    Field ff = new Field();
+    ff.setVisibility(JavaVisibility.PROTECTED);
+    ff.setType(type);
+    ff.setName(field);
+    innerClass.addField(ff);
+    return ff;
+  }
+
+  protected Field generateFieldDeclarationWithGetterSetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    generateGetterSetterFor(field, type, innerClass);
+    return generateFieldDeclarationFor(field, type, innerClass);
+  }
+
+  protected Field generateFieldDeclarationWithFluentApiFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    generateFluentGetterFor(field, type, innerClass);
+    generateFluentSetterFor(field, type, innerClass);
+    return generateFieldDeclarationFor(field, type, innerClass);
+  }
+
+  protected void generateFluentSetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    Method method = new Method();
+    method.setVisibility(JavaVisibility.PUBLIC);
+    method.setName(field);
+    method.addParameter(new Parameter(type, field));
+    method.addBodyLine("this." + field + " = " + field + ";");
+    method.addBodyLine("return this;");
+    method.setReturnType(innerClass.getType());
+    innerClass.addMethod(method);
+  }
+
+  protected void generateFluentGetterFor(String field, FullyQualifiedJavaType type, InnerClass innerClass) {
+    Method method = new Method();
+    method.setVisibility(JavaVisibility.PUBLIC);
+    method.setName(field);
+    method.addBodyLine("return this." + field + ";");
+    method.setReturnType(type);
+    innerClass.addMethod(method);
+  }
+
+  protected void generateBuilderFor(TopLevelClass topLevelClass) {
+    if (topLevelClass == null || topLevelClass.getFields() == null || topLevelClass.getFields().isEmpty()) return;
+    generateBuilderFor(topLevelClass.getType().getShortName(), topLevelClass, topLevelClass.getFields());
+  }
+
+  protected void generateBuilderFor(String builderName, TopLevelClass topLevelClass, List<Field> fields) {
+    if (fields == null || fields.isEmpty()) return;
+
+    FullyQualifiedJavaType builderType = new FullyQualifiedJavaType(builderName);
+
+    InnerClass builder = new InnerClass(builderType);
+    builder.setVisibility(JavaVisibility.PRIVATE);
+    builder.setStatic(false);
+
+    // constructor
+    Method method = new Method();
+    method.setConstructor(true);
+    method.setName(builderName);
+    method.setVisibility(JavaVisibility.PUBLIC);
+    builder.addMethod(method);
+
+    // build method and builder field
+    method = new Method();
+    method.setName("build");
+    method.setVisibility(JavaVisibility.PUBLIC);
+
+    for (Field field : fields) {
+      generateFieldDeclarationWithFluentApiFor(field.getName(), field.getType(), builder);
+      method.addBodyLine(topLevelClass.getType().getShortName() + ".this." + field.getName() + " = " + field.getName() + ";");
+    }
+
+    method.addBodyLine("return " + topLevelClass.getType().getShortName() + ".this;");
+    method.setReturnType(topLevelClass.getType());
+    builder.addMethod(method);
+
+    // builder owner's builder()
+    method = new Method();
+    method.setName(uncapitalize(builderName));
+    method.setVisibility(JavaVisibility.PUBLIC);
+    method.addBodyLine("return new " + builderName + "();");
+    method.setReturnType(builderType);
+    builder.addMethod(method);
+  }
+
+
+  public String capitalize(final String str) {
+    int strLen;
+    if (str == null || (strLen = str.length()) == 0) {
+      return str;
+    }
+
+    char firstChar = str.charAt(0);
+    if (Character.isTitleCase(firstChar)) {
+      // already capitalized
+      return str;
+    }
+
+    return new StringBuilder(strLen)
+        .append(Character.toTitleCase(firstChar))
+        .append(str.substring(1))
+        .toString();
+  }
+
+  public static String uncapitalize(final String str) {
+    int strLen;
+    if (str == null || (strLen = str.length()) == 0) {
+      return str;
+    }
+
+    char firstChar = str.charAt(0);
+    if (Character.isLowerCase(firstChar)) {
+      // already uncapitalized
+      return str;
+    }
+
+    return new StringBuilder(strLen)
+        .append(Character.toLowerCase(firstChar))
+        .append(str.substring(1))
+        .toString();
   }
 }
--- dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/plugin/page/AbstractPaginationPlugin.java
+++ dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/plugin/page/AbstractPaginationPlugin.java
@@ -5,6 +5,7 @@ import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,160 +14,30 @@ import java.util.List;
  * Created by LiuPin on 2015/1/30.
  */
 public abstract class AbstractPaginationPlugin extends AbstractXmbgPlugin {
-    
-    public static final String LIMIT_NAME = "limit";
-    public static final String OFFSET_NAME = "offset";
-    
-    public static final String BOUND_BUILDER_NAME = "BoundBuilder";
 
-    @Override
-    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass,
-                                              IntrospectedTable introspectedTable) {
-        // add field, getter, setter for limit clause
-        addLimit(topLevelClass, introspectedTable, LIMIT_NAME);
-        addLimit(topLevelClass, introspectedTable, OFFSET_NAME);
-        addFluentApi(topLevelClass, introspectedTable, LIMIT_NAME, OFFSET_NAME);
-        return super.modelExampleClassGenerated(topLevelClass,introspectedTable);
-    }
-    
-    /**
-     * 为topLevelClass添加分页的限制属性以及Getter和Setter方法
-     * @author Pin Liu
-     * @编写日期: 2014年11月26日下午12:53:22
-     * @param topLevelClass 带生成的Example类
-     * @param introspectedTable 数据库表的元数据
-     * @param name 属性名
-     */
-    protected void addLimit(TopLevelClass topLevelClass,IntrospectedTable introspectedTable, String name) {
-        CommentGenerator commentGenerator = context.getCommentGenerator();
-        Field field = new Field();
-        field.setVisibility(JavaVisibility.PROTECTED);
-        field.setType(PrimitiveTypeWrapper.getIntegerInstance());
-        field.setName(name);
-        commentGenerator.addFieldComment(field, introspectedTable);
-        topLevelClass.addField(field);
-        String camel = getCamel(name);
-        Method method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setName("set" + camel);
-        method.addParameter(new Parameter(PrimitiveTypeWrapper.getIntegerInstance(), name));
-        method.addBodyLine("this." + name + "=" + name + ";");
-        commentGenerator.addGeneralMethodComment(method, introspectedTable);
-        topLevelClass.addMethod(method);
-        method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setReturnType(PrimitiveTypeWrapper.getIntegerInstance());
-        method.setName("get" + camel);
-        method.addBodyLine("return " + name + ";");
-        commentGenerator.addGeneralMethodComment(method, introspectedTable);
-        topLevelClass.addMethod(method);
-    }
+  public static final String LIMIT_NAME = "limit";
+  public static final String OFFSET_NAME = "offset";
 
-    private String getCamel(String name) {
-        char c = name.charAt(0);
-        return Character.toUpperCase(c) + name.substring(1);
-    }
+  public static final String BOUND_BUILDER_NAME = "BoundBuilder";
 
-    /**
-     * 为topLevelClass添加分页方法控制的Fluent方法 
-     * @param topLevelClass
-     * @param introspectedTable
-     * @param limit
-     * @param offset
-     */
-    protected void addFluentApi(TopLevelClass topLevelClass, IntrospectedTable introspectedTable, String limit, String offset){
+  @Override
+  public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    List<Field> fields = new ArrayList<>();
 
-        InnerClass boundBuilder = generateBoundBuilder(topLevelClass, limit, offset);
-        
-        topLevelClass.addInnerClass(boundBuilder);
-        
-        Method method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setName("bound");
-        method.setReturnType(boundBuilder.getType());
-        method.addBodyLine("return new "+BOUND_BUILDER_NAME+"(this);");
-        
-        topLevelClass.addMethod(method);
-    }
+    // add field, getter, setter for limit clause
+    Field field = generateFieldDeclarationWithGetterSetterFor(LIMIT_NAME, FullyQualifiedJavaType.getIntInstance(), topLevelClass);
+    fields.add(field);
+    field = generateFieldDeclarationWithGetterSetterFor(OFFSET_NAME, FullyQualifiedJavaType.getIntInstance(), topLevelClass);
+    fields.add(field);
 
-    /**
-     * 生成边界建造器对象
-     * @param topLevelClass
-     * @param limit
-     * @param offset
-     * @return
-     */
-    private InnerClass generateBoundBuilder(TopLevelClass topLevelClass, String limit, String offset) {
-        FullyQualifiedJavaType boundBuilderType = new FullyQualifiedJavaType(BOUND_BUILDER_NAME);
+    generateBuilderFor(BOUND_BUILDER_NAME, topLevelClass, fields);
+    return true;
+  }
 
-        InnerClass boundBuilder = new InnerClass(boundBuilderType);
-        boundBuilder.setVisibility(JavaVisibility.PUBLIC);
-        boundBuilder.setStatic(true);
-
-        Field field = new Field();
-        field.setVisibility(JavaVisibility.PRIVATE);
-        field.setType(PrimitiveTypeWrapper.getIntegerInstance());
-        field.setName(limit);
-        boundBuilder.addField(field);
-
-        field = new Field();
-        field.setVisibility(JavaVisibility.PRIVATE);
-        field.setType(PrimitiveTypeWrapper.getIntegerInstance());
-        field.setName(offset);
-        boundBuilder.addField(field);
-
-        field = new Field();
-        field.setVisibility(JavaVisibility.DEFAULT);
-        field.setType(topLevelClass.getType());
-        field.setName("target");
-        boundBuilder.addField(field);
-
-        // 构造方法
-        Method method = new Method();
-        method.setConstructor(true);
-        method.setName(BOUND_BUILDER_NAME);
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.addParameter(new Parameter(topLevelClass.getType(),"target"));
-        method.addBodyLine("this.target = target;");
-        boundBuilder.addMethod(method);
-        
-        // 设置limit的方法
-        method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setName(limit);
-        method.addParameter(new Parameter(PrimitiveTypeWrapper.getIntegerInstance(), limit));
-        method.setReturnType(boundBuilderType);
-        method.addBodyLine("this." + limit + "=" + limit + ";");
-        method.addBodyLine("return this;");
-        boundBuilder.addMethod(method);
-
-        // 设置offset的方法
-        method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setName(offset);
-        method.addParameter(new Parameter(PrimitiveTypeWrapper.getIntegerInstance(), offset));
-        method.setReturnType(boundBuilderType);
-        method.addBodyLine("this." + offset + "=" + offset + ";");
-        method.addBodyLine("return this;");
-        boundBuilder.addMethod(method);
-
-        // 将搜集到的page相关属性设置到target中
-        method = new Method();
-        method.setVisibility(JavaVisibility.PUBLIC);
-        method.setName("build");
-        method.setReturnType(topLevelClass.getType());
-        method.addBodyLine("this.target.set" + getCamel(limit) + "(" + limit + ");");
-        method.addBodyLine("this.target.set" + getCamel(offset) + "(" + offset + ");");
-        method.addBodyLine("return this.target;");
-        boundBuilder.addMethod(method);
-        
-        return boundBuilder;
-    }
-
-    /*
-     * This plugin is always valid - no properties are required
-     */
-    public boolean validate(List<String> warnings) {
-        return true;
-    }
+  /**
+   * This plugin is always valid - no properties are required
+   */
+  public boolean validate(List<String> warnings) {
+    return true;
+  }
 }
