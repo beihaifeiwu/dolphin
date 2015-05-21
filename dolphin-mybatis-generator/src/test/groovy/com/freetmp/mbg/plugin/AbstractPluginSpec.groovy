90eb6a0036f03ca3f6403b7d89d0647410a1cd74
liu pin <pin.liu@palmaplus.com>
2015-05-21 14:45:54 +0800

    make mock and stup active

--- dolphin-mybatis-generator/src/test/groovy/com/freetmp/mbg/plugin/AbstractPluginSpec.groovy
+++ dolphin-mybatis-generator/src/test/groovy/com/freetmp/mbg/plugin/AbstractPluginSpec.groovy
@@ -1,5 +1,6 @@
 package com.freetmp.mbg.plugin
 
+import org.apache.log4j.BasicConfigurator
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.SystemOutRule
 import org.mybatis.generator.api.IntrospectedColumn
@@ -18,54 +19,49 @@ import spock.lang.Specification
  */
 abstract class AbstractPluginSpec extends Specification {
 
-  @Rule SystemOutRule systemOutRule = new SystemOutRule().muteForSuccessfulTests()
+  @Rule SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests()
 
-  Interface interfaze = Spy(Interface, constructorArgs: [User.class.canonicalName + "Mapper"])
+  Interface interfaze = Mock(Interface, constructorArgs: [User.class.canonicalName + "Mapper"])
   TopLevelClass topLevelClass = Mock()
+  XmlElement root = Mock(XmlElement, constructorArgs: ["mapper"])
+  Document document = Mock()
+  Rules rules = Stub()
+  TableConfiguration tableConfiguration = Stub()
+  IntrospectedTable introspectedTable = Stub()
 
-  Document document = Spy() {
-    document.getRootElement() >> root
-  }
-  XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
+  List<IntrospectedColumn> introspectedColumns = []
 
-  Rules rules = Stub() {
+  List<IntrospectedColumn> introspectedNpkColumns = []
+
+  List<IntrospectedColumn> introspectedPkColumns = []
+
+  def setup(){
+    document.rootElement >> root
     rules.calculateAllFieldsClass() >> new FullyQualifiedJavaType(User.class.canonicalName)
-  }
-
-  TableConfiguration tableConfiguration = Stub() {
     tableConfiguration.getDomainObjectName() >> User.class.simpleName
-  }
 
-  IntrospectedTable introspectedTable = Stub() {
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "id" }; getJavaProperty() >> "id"; getJdbcTypeName() >> "BIGINT"; getActualColumnName() >> "id"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "loginName" }; getJavaProperty() >> "loginName"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "login_Name"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "name" }; getJavaProperty() >> "name"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "name"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "password" }; getJavaProperty() >> "password"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "password"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "salt" }; getJavaProperty() >> "salt"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "salt"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "roles" }; getJavaProperty() >> "roles"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "roles"; isColumnNameDelimited() >> false }
+    introspectedColumns << Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "registerDate" }; getJavaProperty() >> "registerDate"; getJdbcTypeName() >> "TIMESTAMP"; getActualColumnName() >> "register_date"; isColumnNameDelimited() >> false }
+
+    introspectedNpkColumns.addAll introspectedColumns[1..-1]
+    introspectedPkColumns << introspectedColumns[0]
+
     introspectedTable.getRules() >> rules
     introspectedTable.getTableConfiguration() >> tableConfiguration
     introspectedTable.getAllColumns() >> introspectedColumns
     introspectedTable.getPrimaryKeyColumns() >> introspectedPkColumns
     introspectedTable.getNonPrimaryKeyColumns() >> introspectedNpkColumns
     introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime() >> "user"
+
   }
 
-  List<IntrospectedColumn> introspectedColumns = [
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "id" }; getJavaProperty() >> "id"; getJdbcTypeName() >> "BIGINT"; getActualColumnName() >> "id"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "loginName" }; getJavaProperty() >> "loginName"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "login_Name"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "name" }; getJavaProperty() >> "name"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "name"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "password" }; getJavaProperty() >> "password"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "password"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "salt" }; getJavaProperty() >> "salt"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "salt"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "roles" }; getJavaProperty() >> "roles"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "roles"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "registerDate" }; getJavaProperty() >> "registerDate"; getJdbcTypeName() >> "TIMESTAMP"; getActualColumnName() >> "register_date"; isColumnNameDelimited() >> false }
-  ]
-
-  List<IntrospectedColumn> introspectedNpkColumns = [
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "loginName" }; getJavaProperty() >> "loginName"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "login_Name"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "name" }; getJavaProperty() >> "name"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "name"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "password" }; getJavaProperty() >> "password"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "password"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "salt" }; getJavaProperty() >> "salt"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "salt"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "roles" }; getJavaProperty() >> "roles"; getJdbcTypeName() >> "VARCHAR"; getActualColumnName() >> "roles"; isColumnNameDelimited() >> false },
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "registerDate" }; getJavaProperty() >> "registerDate"; getJdbcTypeName() >> "TIMESTAMP"; getActualColumnName() >> "register_date"; isColumnNameDelimited() >> false }
-  ]
-
-  List<IntrospectedColumn> introspectedPkColumns = [
-      Mock(IntrospectedColumn) { getJavaProperty(_) >> { String prefix -> prefix + "id" }; getJavaProperty() >> "id"; getJdbcTypeName() >> "BIGINT"; getActualColumnName() >> "id"; isColumnNameDelimited() >> false },
-  ]
+  def setupSpec(){
+    BasicConfigurator.configure()
+  }
 
 }
--- dolphin-mybatis-generator/src/test/groovy/com/freetmp/mbg/plugin/UpsertPluginSpec.groovy
+++ dolphin-mybatis-generator/src/test/groovy/com/freetmp/mbg/plugin/UpsertPluginSpec.groovy
@@ -1,6 +1,7 @@
 package com.freetmp.mbg.plugin
 
 import com.freetmp.mbg.plugin.upsert.*
+import groovy.util.logging.Slf4j
 import org.mybatis.generator.api.IntrospectedTable
 import org.mybatis.generator.api.dom.java.Interface
 import org.mybatis.generator.api.dom.java.Method
@@ -12,29 +13,22 @@ import org.mybatis.generator.api.dom.xml.XmlElement
 /**
  * Created by LiuPin on 2015/5/20.
  */
+@Slf4j
 class UpsertPluginSpec extends AbstractPluginSpec {
 
   def "check generated method signature"() {
     setup:
-    AbstractUpsertPlugin plugin = new AbstractUpsertPlugin() {
-      @Override
-      protected void generateSqlMapContent(IntrospectedTable introspectedTable, XmlElement parent) {}
-
-      @Override
-      protected void generateSqlMapContentSelective(IntrospectedTable introspectedTable, XmlElement parent) {}
-    }
+    AbstractUpsertPlugin plugin = Spy()
 
     when:
     plugin.clientGenerated(interfaze, topLevelClass, introspectedTable)
 
     then:
-    with(interfaze){
-      1 * addMethod { Method method -> method.getFormattedContent(0, true) == "int upsert(@Param(\"record\") User record, @Param(\"array\") String[] array);" }
-      1 * addMethod { Method method -> method.getFormattedContent(0, true) == "int upsertSelective(@Param(\"record\") User record, @Param(\"array\") String[] array);" }
-      1 * addMethod { Method method -> method.getFormattedContent(0, true) == "int batchUpsert(@Param(\"records\") List<User> list, @Param(\"array\") String[] array);" }
-      1 * addMethod { Method method -> method.getFormattedContent(0, true) == "int batchUpsertSelective(@Param(\"records\") List<User> list, @Param(\"array\") String[] array);" }
-      1 * addImportedTypes({ it.size() >= 3 })
-    }
+    1 * interfaze.addMethod { Method method -> method.getFormattedContent(0, true) == "int upsert(@Param(\"record\") User record, @Param(\"array\") String[] array);" }
+    1 * interfaze.addMethod { Method method -> method.getFormattedContent(0, true) == "int upsertSelective(@Param(\"record\") User record, @Param(\"array\") String[] array);" }
+    1 * interfaze.addMethod { Method method -> method.getFormattedContent(0, true) == "int batchUpsert(@Param(\"records\") List<User> list, @Param(\"array\") String[] array);" }
+    1 * interfaze.addMethod { Method method -> method.getFormattedContent(0, true) == "int batchUpsertSelective(@Param(\"records\") List<User> list, @Param(\"array\") String[] array);" }
+    1 * interfaze.addImportedTypes({ it.size() >= 3 })
   }
 
   def "check generated upsert series xml for mysql"() {
@@ -45,76 +39,62 @@ class UpsertPluginSpec extends AbstractPluginSpec {
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
+    _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0)}
+
   }
 
   def "check generated upsert series xml for oracle"() {
     setup:
     OracleUpsertPlugin plugin = new OracleUpsertPlugin();
-    Document document = Spy()
-    XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
 
     when:
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * document.rootElement >> root
     _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
   }
 
   def "check generated upsert series xml for postgresql"() {
     setup:
     PostgreSQLUpsertPlugin plugin = new PostgreSQLUpsertPlugin();
-    Document document = Spy()
-    XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
 
     when:
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * document.rootElement >> root
     _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
   }
 
   def "check generated upsert series xml for sqlserver"() {
     setup:
     SQLServerUpsertPlugin plugin = new SQLServerUpsertPlugin();
-    Document document = Spy()
-    XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
 
     when:
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * document.rootElement >> root
     _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
   }
 
   def "check generated upsert series xml for hsqldb"() {
     setup:
     HsqldbUpsertPlugin plugin = new HsqldbUpsertPlugin();
-    Document document = Spy()
-    XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
 
     when:
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * document.rootElement >> root
     _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
   }
 
   def "check generated upsert series xml for db2"() {
     setup:
     DB2UpsertPlugin plugin = new DB2UpsertPlugin();
-    Document document = Spy()
-    XmlElement root = Spy(XmlElement, constructorArgs: ["mapper"])
 
     when:
     plugin.sqlMapDocumentGenerated(document, introspectedTable)
 
     then:
-    _ * document.rootElement >> root
     _ * root.addElement(_) >> { Element element -> println element.getFormattedContent(0) }
   }
 
