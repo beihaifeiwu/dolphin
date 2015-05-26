84d3ce245f61b4ddaac2a9a1caf1cb9a348cf7bd
liupin <liuhuizhang@foxmail.com>
2015-05-26 23:09:36 +0800

    use plugin to resolve batch problems

---
+++ dolphin-mybatis-generator/src/main/java/com/freetmp/mbg/interceptor/BatchInterceptor.java
@@ -0,0 +1,35 @@
+package com.freetmp.mbg.interceptor;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+/**
+ * Created by pin on 2015/5/26.
+ */
+@Intercepts({
+    @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class BatchInterceptor implements Interceptor {
+
+  @Override
+  public Object intercept(Invocation invocation) throws Throwable {
+    return null;
+  }
+
+  @Override
+  public Object plugin(Object target) {
+    if (target instanceof Executor) {
+      return Plugin.wrap(target, this);
+    } else {
+      return target;
+    }
+  }
+
+  @Override
+  public void setProperties(Properties properties) {
+
+  }
+}
